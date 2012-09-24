55d3c8e6956e66d138f4953f55910d3203cea8b0
Rajeev Thiruvengadam <veejar2020@Rajeevs-MacBook-Pro.local>
2012-09-24 20:45:34 +0530

    2

--- cookbooks/rails_downgrade/recipes/default.rb
+++ cookbooks/rails_downgrade/recipes/default.rb
@@ -1,3 +1,5 @@
-execute "downgrade rubygems" do
-command "gem update --system 1.5.2"
-end
+if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
+	execute "downgrade rubygems" do
+	command "gem update --system 1.5.2"
+	end
+end	
